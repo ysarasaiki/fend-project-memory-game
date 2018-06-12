2989a75c1a7375098a3cf5ce69945c5e2e4fb512
Yuna Saiki <ysaiki.529@gmail.com>
2018-06-11 19:17:17 -0700

    Add shuffling functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 /*
  * Create a list that holds all of your cards
  */
+const icons = ["flaticon-002-salt-and-pepper", "flaticon-001-bacon", "flaticon-005-sausage", "flaticon-015-beer-can", "flaticon-027-pinwheel", "flaticon-018-taco", "flaticon-034-flower", "flaticon-032-ladybug"]
 
+const cards = new Array();
+for (const icon of icons) {
+	cards.push(icon);
+	cards.push(icon);
+}
 
-/*
- * Display the cards on the page
+/* Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
@@ -24,12 +29,28 @@ function shuffle(array) {
 
     return array;
 }
+shuffle(cards);
 
+const fragment = document.createDocumentFragment();
+for (const card of cards) {
+	const newCard = document.createElement('li');
+	newCard.className = 'card'
+
+	const newIcon = document.createElement('i');
+	newIcon.className = card;
+
+	newCard.appendChild(newIcon);
+	fragment.appendChild(newCard);
+}
+
+document.querySelector('.deck').appendChild(fragment);
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*/
+/* - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)*/
+/*
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
